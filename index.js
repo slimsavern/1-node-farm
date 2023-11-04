6364277df103dfce5527ba691bbb7cecdd7c25f7
Elias Bansi <slimsavern@gmail.com>
2023-11-04 22:53:11 +0000

    API implementation

--- index.js
+++ index.js
@@ -1,32 +1,66 @@
-const fs = require('fs');
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
+//////////////////////////////////////
+////FILES
 
-// Blocking, synchronous way
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// // Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
+// // console.log(textIn);
 
-// console.log(textIn);
+// // const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}`;
+// // fs.writeFileSync('./txt/output.txt', textOut)
 
-// const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt', textOut)
+// // console.log('File Writen')
 
-// console.log('File Writen')
+// // Non-blocking, asynchronous wa
 
-// Non-blocking, asynchronous wa
+// fs.readFile("./txt/start.txt", 'utf-8', (err, data1) => {
+//    //console.log(data)
+//     if (err) return console.log('Error 💣')
 
-fs.readFile("./txt/start.txt", 'utf-8', (err, data1) => {
-   //console.log(data)
-    
-    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-		// console.log(data2);
+//     fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+// 		// console.log(data2);
 
-        fs.readFile(`./txt/append.txt`, 'utf-8', (err,data3) => {
-            console.log(data3)
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err,data3) => {
+//             console.log(data3)
 
-            fs.writeFile('./txt/final.txt', `${data2}\n ${data3}`,'utf-8', (err) => {
-                console.log("Your file has been written🎉")
-            } )
-        });
-    });
+//             fs.writeFile('./txt/final.txt', `${data2}\n ${data3}`,'utf-8', (err) => {
+//                 console.log("Your file has been written🎉")
+//             } )
+//         });
+//     });
+// });
+// console.log("Will read file!")
+
+//////////////////////////////////////
+//// SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+	const pathName = req.url;
+
+	if (pathName === "/overview" || pathName === "/") {
+		res.end("THis is the Overview");
+	} else if (pathName === "/product") {
+		res.end("This is the product page");
+	} else if (pathName == "/api") {
+		res.writeHead(200, {
+			"Content-type": "application/json",
+		});
+		res.end(data);
+	} else {
+		res.writeHead(404, {
+			"Content-type": "text/html",
+			"my-own-header": "hello-world",
+		});
+		res.end("<h1> Page Not Found! </h1>");
+	}
 });
-console.log("Will read file!")
+
+server.listen(8000, "127.0.0.1", () => {
+	console.log("Listening to requests on port 8000");
+});
