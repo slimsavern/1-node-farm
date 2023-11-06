87517871711d640e0b0c88b790899311807406a8
Elias Bansi <slimsavern@gmail.com>
2023-11-06 19:16:29 +0000

    Modules demo

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-
-//////////////////////////////////////
+const replaceTemplate = require("./modules/replaceTemplate");
+/////////////////// ///////////////////
 ////FILES
 
 // // Blocking, synchronous way
@@ -44,20 +44,6 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, "utf-
 
 const dataObj = JSON.parse(data);
 
-const replaceTemplate = (temp, product) => {
-	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-	output = output.replace(/{%IMAGE%}/g, product.image);
-	output = output.replace(/{%PRICE%}/g, product.price);
-	output = output.replace(/{%FROM%}/g, product.from);
-	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-	output = output.replace(/{%QUANTITY%}/g, product.quantity);
-	output = output.replace(/{%DESCRIPTION%}/g, product.description);
-	output = output.replace(/{%ID%}/g, product.id);
-
-	if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-	return output;
-};
 const server = http.createServer((req, res) => {
 	// console.log(req.url);
 	// console.log(url.parse(req.url, true));
